6a65637d6abe7b2fb487d287c56a6436994dc0ba
Prachi Pawar <136091643+PrachiPawar23@users.noreply.github.com>
2023-10-29 20:56:03 +0530

    devstar_timezoneconverter

--- src/routes/(tools)/converter_timezone/+page.svelte
+++ src/routes/(tools)/converter_timezone/+page.svelte
@@ -1,147 +1,276 @@
-
 <script>
-// @ts-nocheck
-
-    import './script'
-</script>
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<style>
+    let timezoneCount = 0;
+    let currentDateTime = new Date().toLocaleString('sv');
+    currentDateTime = currentDateTime.substring(0, currentDateTime.length - 3);
+    /**
+	 * @type {any[]}
+	 */
+    let selectedTimezones = [];
+    /**
+	 * @type {string | any[]}
+	 */
+    let convertedTimes = [];
+  
+    function addTimezone() {
+      if (timezoneCount < 5) {
+        selectedTimezones = [...selectedTimezones, ''];
+        timezoneCount++;
+      }
+    }
+  
+    let sortedTimezones = [
+    "UTC",
+    "America/New_York",
+    "Europe/London",
+    "Asia/Tokyo",
+    "Australia/Sydney",
+    "America/Los_Angeles",
+    "Asia/Dubai",
+    "Europe/Paris",
+    "Africa/Cairo",
+    "Asia/Singapore",
+    // Add more timezones here
+  ].sort();
+  
+    /**
+	 * @param {number} index
+	 */
+    function deleteTimezone(index) {
+      selectedTimezones.splice(index, 1);
+      timezoneCount--;
+      selectedTimezones = [...selectedTimezones]; // trigger Svelte reactivity
+    }
+  
+    function convertTimezones() {
+      const datetime = new Date(currentDateTime);
+      /**
+		 * @type {any[]}
+		 */
+      const resultText = [];
+  
+      selectedTimezones.forEach((timezone) => {
+        if (timezone) {
+          const convertedDate = new Date(
+            datetime.toLocaleString('en-US', { timeZone: timezone })
+          );
+          resultText.push({
+            timezone,
+            convertedDate: convertedDate.toLocaleString(),
+          });
+        }
+      });
+  
+      convertedTimes = resultText;
+    }
+  </script>
+  <style>
     body {
-        font-family: Franklin Gothic Medium, sans-serif;
-        background-color: #F4F4F4;
-        background-size: 1200px;
-        height: 100%;
-        width: 100%;
-        margin: 0;
-        padding: 0;
+    font-family: Franklin Gothic Medium, sans-serif;
+    background-color: transparent;
+    background-image: url('world.png');
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-position: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 0;
+  }
+  
+    #title {
+      text-align: center;
     }
-
+  
     h3 {
-        color: #00313d;
-        text-shadow: #006e8a;
+      color: #00313D;
     }
-
+  
     #shape {
-        background: #00a9f7c2;
-        text-align: center;
-        border: 1px solid #9CE7FA;
-        border-radius: 10px;
-        box-shadow: 2px 2px 10px rgba(0, 112, 139, 0.37);
-        padding: 20px;
-        height: auto;
-        width: auto ;
-        margin: 10px;
+      align-self: center;
+      background: #00a9f7c2;
+      text-align: center;
+      border: 1px solid #9CE7FA;
+      border-radius: 10px;
+      box-shadow: 2px 2px 10px rgba(0, 112, 139, 0.37);
+      padding: 20px;
+      width: 100%;
+      max-width: 400px;
+      margin: 10px;
     }
-
+  
     .button1 {
-        float: left;
-        padding: 5px;
-        background-color: paleturquoise;
-        border-radius: 10px;
-        border-color: #fcfeffc2;
-        align-content: center;
-        width: 35%;
+      align-self: center;
+      padding: 10px;
+      background-color: paleturquoise;
+      border-radius: 10px;
+      border: 1px solid #fcfeffc2;
+      width: 40%;
+      max-width: 400px;
+      margin-bottom: 10%;
+      float: left;
     }
-
+  
+  
     .button2 {
-        float: right;
-        padding: 5px;
-        background-color: paleturquoise;
-        border-radius: 10px;
-        border-color: #fcfeffc2;
-        align-content: center;
-        width: 55%;
+      align-self: center;
+      padding: 10px;
+      background-color: paleturquoise;
+      border-radius: 10px;
+      border: 1px solid #fcfeffc2;
+      width: 55%;
+      max-width: 400px;
+      margin-bottom: 10%;
+      float: right;
     }
-
+  
     .timezone-box {
-        background-color: #73bbfdc2;
-        color: rgb(0, 64, 80);
-        padding: 10px;
-        border-radius: 5px;
-        margin: 10px;
+      background-color: #73bbfdc2;
+      color: rgb(0, 64, 80);
+      padding: 10px;
+      border-radius: 5px;
+      margin: 10px;
+      width: auto;
+      max-width: 400px;
+      align-self: center;
     }
-
+  
     #datetime {
-        border-radius: 5px;
-        border-color:#ffffff;
+      border-radius: 5px;
+      border-color: #FFFFFF;
+      width: 100%;
+      max-width: 400px;
+      margin: 10px;
     }
-
+  
     #container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-top: 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      margin-top: 50px;
     }
-
+  
     .timezone {
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        margin-bottom: 20px;
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      margin-bottom: 20px;
+      width: 100%;
+      max-width: 400px;
     }
-
+  
     .timezone label {
-        margin-right: 10px;
+      margin-right: 10px;
     }
-
+  
     .deleteBtn {
-        margin: auto;
-        margin-left: 10px;
-        padding: 5px 10px;
-        background-color: rgb(255, 124, 100);
-        border-radius: 5px;
-        color: #ffffff;
-        border: none;
-        cursor: pointer;
+      padding: 10px;
+      background-color: rgb(255, 124, 100);
+      border-radius: 5px;
+      color: #FFFFFF;
+      border: none;
+      cursor: pointer;
     }
-
+  
     #result {
-        margin-top: 20px;
-        font-size: 20px;
+      margin-top: 20px;
+      font-size: 20px;
+      width: 100%;
+      max-width: 400px;
     }
-
+  
     .result-box {
-        text-align: center;
-        background-color: #73bbfdc2;
-        color: rgb(0, 64, 80);
-        padding: 10px;
-        border-radius: 10px;
-        margin: 10px;
+      text-align: center;
+      background-color: #73bbfdc2;
+      color: rgb(0, 64, 80);
+      padding: 10px;
+      border-radius: 10px;
+      margin: 10px;
+      width: 100%;
+      max-width: 400px;
     }
-
+  
     @media only screen and (max-width: 600px) {
-        /* Adjust styles for smaller screens */
-        .timezone {
-            flex-direction: column;
-            align-items: flex-start;
-        }
-
-        .timezone label {
-            margin-bottom: 10px;
-        }
+      /* Adjust styles for smaller screens */
+      .timezone {
+        flex-direction: column;
+        align-items: flex-start;
+      }
+      .timezone label {
+        margin-bottom: 10px;
+      }
     }
-
-</style>
-
-<body background="world.png">
-<div id="container">
-    <h1 style="color: #00313d;">Timezone Converter</h1>
-    <div id="timezones">
+  
+    #timezones {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0%;
+    }
+  
+    #datetime {
+      flex: 1;
+      border-radius: 5px;
+      border-color: #FFFFFF;
+      max-width: calc(100% - 100px);
+      margin: 10px 5px;
+    }
+  
+  </style>
+  
+  <body>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <h1 style="color: #22859E;" id="title">Timezone Converter</h1>
+    <div id="container">
+      <div id="timezones">
         <div class="timezone" id="shape">
-            <label for="datetime"><h3>Date and Time:</h3></label>
-            <input type="datetime-local" id="datetime" value="">
+          <label for="datetime">
+            <h3>Date and Time:</h3>
+          </label>
+          <input type="datetime-local" bind:value={currentDateTime} id="datetime">
         </div>
-        
+      </div>
+      <div>
+        <div id="addedTimezones">
+          {#each selectedTimezones.sort() as timezone, index}
+  <div class="timezone-box">
+    <label for={"timezone" + index}>Timezone {index + 1}:</label>
+    <select bind:value={selectedTimezones[index]}>
+      <option value="" disabled>Select timezone</option>
+      {#each sortedTimezones as sortedTimezone}
+        <option value={sortedTimezone}>{sortedTimezone}</option>
+      {/each}
+    </select>
+    <button on:click={() => deleteTimezone(index)} class="deleteBtn">
+      <i class="fas fa-trash"></i>
+    </button>
+  </div>
+  {/each}
+        </div>
+        <button on:click={addTimezone} class="button1">
+          <h3>Add Timezone</h3>
+        </button>
+        <button on:click={convertTimezones} class="button2">
+          <h3>Convert Timezones</h3>
+        </button>
+      </div>
+      <div id="result">
+        {#if convertedTimes.length > 0}
+        <div class="result-box">
+          <h3>Converted Times:</h3>
+          <ul>
+            {#each convertedTimes as { timezone, convertedDate }}
+            <li>
+              <strong>{timezone}:</strong> {convertedDate}
+            </li>
+            {/each}
+          </ul>
+        </div>
+        {/if}
+      </div>
     </div>
-    <div>
-        <div id="addedTimezones"></div>
-        <button onclick="addTimezone()" id="addButton" class="button1"><h3>Add Timezone</h3></button>
-
-        <button onclick="convertTimezones()" class="button2"><h3>Convert Timezones</h3></button>
-    </div>
-    <div id="result"></div>
-</div>
-
-
-</body>
+  </body>
